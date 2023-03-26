3c13335bffe4c6d507c14cd98a990646d98f5b71
Silviu <128051262+SilviuA96@users.noreply.github.com>
2023-03-26 18:16:51 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_Expressions_Alert/JS/main.js
@@ -0,0 +1,5 @@
+var blues = "I have the blues"; //Adding text
+var blues = blues.fontcolor("red"); //Making the font blue
+document.write (blues); //Write the text
+var B = "Hello Instructor";
+window.alert (B)
