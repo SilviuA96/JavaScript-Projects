4a0e3c3ebf7e57c282d1374aa321ceeaccc94ecb
Silviu <128051262+SilviuA96@users.noreply.github.com>
2023-03-26 19:11:36 +0100

    Add files via upload

---
+++ Project2_functions/JS/two.js
@@ -0,0 +1,5 @@
+function myFunction() { //Function
+  var sentence = "I am learning"; // Text
+  sentence += " JavaScript and it`s fun! "; //Text
+  document.getElementById("Concatenate") .innerHTML = sentence;
+}
